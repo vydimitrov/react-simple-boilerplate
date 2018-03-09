fc74ad44bc1e0401958c8e195676fadca539b317
Vasil Dimitrov <vasil@twentythree.net>
2018-03-09 09:37:37 +0100

    React 16.3alpha

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <h1 className="test-ie11-bug">Hello React :)</h1>
     );
   }
 }
